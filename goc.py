e0a9625fec9ff4b4e3d1fdd68e13b7dc11ff3412
Matthieu Barba <mat.barba@gmail.com>
2024-03-07 19:32:38 +0000

    Simpler strings, drop goc table

--- goc.py
+++ goc.py
@@ -23,6 +23,7 @@ conn = sqlite3.connect(db_file)
 
 cur = conn.cursor()
 
+cur.execute("""DROP TABLE goc""")
 cur.execute("""CREATE TABLE goc(sp1 TEXT, sp2 TEXT, pos INTEGER, score REAL)""")
 
 # Get all species in the database
@@ -42,38 +43,45 @@ for ref in list_species:
 
         # Get all CDS for the reference
         cur.execute(
-            "SELECT g.pid FROM genes g JOIN genome_parts gp ON g.gpart = gp.gpart WHERE feat = 'CDS' and g.sp = ? ORDER BY gp.min, loc_start ASC;",
+            """
+            SELECT g.pid FROM genes g JOIN genome_parts gp ON g.gpart = gp.gpart
+            WHERE feat = 'CDS' and g.sp = ?
+            ORDER BY gp.min, loc_start ASC;
+            """,
             (ref,),
         )
         list_cds_ref = [x[0] for x in cur.fetchall()]
 
         # Get all genes for the reference
         cur.execute(
-            "SELECT g.pid FROM genes g JOIN genome_parts gp ON g.gpart = gp.gpart WHERE g.sp = ? ORDER BY gp.min, loc_start ASC;",
+            """
+            SELECT g.pid FROM genes g JOIN genome_parts gp ON g.gpart = gp.gpart
+            WHERE g.sp = ?
+            ORDER BY gp.min, loc_start ASC;
+            """,
             (ref,),
         )
         list_gene_ref = [x[0] for x in cur.fetchall()]
 
         # Get all CDS for the target
         cur.execute(
-            "SELECT g.pid FROM genes g JOIN genome_parts gp ON g.gpart = gp.gpart WHERE feat = 'CDS' and g.sp = ? ORDER BY gp.min, loc_start ASC;",
+            """
+            SELECT g.pid FROM genes g JOIN genome_parts gp ON g.gpart = gp.gpart
+            WHERE feat = 'CDS' and g.sp = ?
+            ORDER BY gp.min, loc_start ASC
+            """,
             (tar,),
         )
         list_cds_tar = [x[0] for x in cur.fetchall()]
 
-        # SQL request preparation
-        sql_prep_cds_list = "'"
-        sql_prep_cds_list = sql_prep_cds_list + ("', '").join(list_cds_ref) + "'"
-        sql_handler = (
-            "SELECT g1.pid gene_id1, g2.pid gene_id2 FROM orthos o JOIN genes g1 ON o.pid1 = g1.pid JOIN genes g2 ON o.pid2 = g2.pid WHERE o.pid1 IN ("
-            + sql_prep_cds_list
-            + ") AND g2.sp = '"
-            + tar
-            + "';"
-        )
+        # Get all ortholog pairs
+        sql_prep_cds_list = "'" + ("', '").join(list_cds_ref) + "'"
+        sql_handler = f"""
+            SELECT g1.pid gene_id1, g2.pid gene_id2
+            FROM orthos o JOIN genes g1 ON o.pid1 = g1.pid JOIN genes g2 ON o.pid2 = g2.pid
+            WHERE o.pid1 IN ({sql_prep_cds_list}) AND g2.sp = '{tar}'
+            """
         cur.execute(sql_handler)
-
-        # Data preparation
         ort = {}
         raw_sql_ort = cur.fetchall()
         for i in raw_sql_ort:
@@ -124,17 +132,7 @@ for ref in list_species:
             loc_start_window += 1
 
             # Insert a row of data
-            handler = (
-                "INSERT INTO goc VALUES ('"
-                + ref
-                + "','"
-                + tar
-                + "','"
-                + str(goc_loc)
-                + "','"
-                + str(goc_cds)
-                + "')"
-            )
+            handler = f"INSERT INTO goc VALUES ('{ref}','{tar}', '{goc_loc}','{goc_cds}')"
             cur.execute(handler)
         # Save the changes
         conn.commit()
